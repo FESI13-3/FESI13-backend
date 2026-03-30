c4c6fdd5c4d35ce243086cba7f1424c9860edc42
Ahnyeongjun <toinin@naver.com>
2026-03-30 15:17:37 +0900

    feat: extend repositories for membership queries

--- src/main/java/com/fesi/deadlinemate/domain/gathering/repository/GatheringRepository.java
+++ src/main/java/com/fesi/deadlinemate/domain/gathering/repository/GatheringRepository.java
@@ -1,7 +1,15 @@
 package com.fesi.deadlinemate.domain.gathering.repository;
 
 import com.fesi.deadlinemate.domain.gathering.entity.Gathering;
+import com.fesi.deadlinemate.domain.gathering.entity.GatheringStatus;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GatheringRepository extends JpaRepository<Gathering,Long>,GatheringRepositoryCustom {
+public interface GatheringRepository extends JpaRepository<Gathering, Long>, GatheringRepositoryCustom {
+
+    Page<Gathering> findByIdInOrderByCreatedAtDesc(List<Long> ids, Pageable pageable);
+
+    Page<Gathering> findByIdInAndStatusOrderByCreatedAtDesc(List<Long> ids, GatheringStatus status, Pageable pageable);
 }
